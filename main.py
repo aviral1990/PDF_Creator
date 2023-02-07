80ce7b9a901e181559eba32ce138bc95a2d0f558
Aviral-PC <aviral17@hotmail.com>
2023-02-07 12:54:10 +0530

    PDF Creation with given number of pages

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from fpdf import FPDF
 FILENAME="topics.csv"
 
 df=pandas.read_csv(FILENAME)
+#ctreate pdf object with properties
+pdf=FPDF(orientation="P",unit="mm",format="A4")
+pdf.set_font(family="Times",style="B",size=20)
+
 
 for index,row in df.iterrows():
-    print(row["Topic"])
+    pdf.add_page()
+    page=1
+    # width of cell,height of cell,text,border=1 or 0,ln is new line
+    pdf.cell(w=0, h=12, txt=row["Topic"], border=0, ln=1, align='L')
+    pdf.cell(w=0, h=0, txt='', border=1, ln=1, align='L')
+    #pdf.footer(row["Topic"])
+    while page < int(row["Pages"]):
+        pdf.add_page()
+        page=page+1
 
 
-pdf=FPDF(orientation="P",unit="mm",format="A4")
-
-pdf.add_page()
-
-pdf.set_font(family="Times",style="B",size=12)
-#width of cell,height of cell,text,border=1 or 0,ln is new line
-pdf.cell(w=0,h=12,txt='Hello There',border=1,ln=1,align='L')
+#Output PDF
 pdf.output("Output.pdf")
